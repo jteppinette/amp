4790b78e6271d711d93d6dfe20e795ef296cf0d1
Joshua Taylor Eppinette <josheppinette@icloud.com>
2014-07-28 22:10:37 +0000

    Added breadcrumbs.

--- app/urls.py
+++ app/urls.py
@@ -8,19 +8,19 @@ from app import views
 
 urlpatterns = patterns('',
     # Authentication
-    url(r'^login/$', views.auth_login),
-    url(r'^logout/$', views.auth_logout),
+    url(r'^login/$', views.auth_login, name="auth_login"),
+    url(r'^logout/$', views.auth_logout, name="auth_login"),
     
     # General
-    url(r'^$', views.home),
+    url(r'^$', views.home, name="home"),
 
     # App
-    url(r'^dashboard/$', views.dashboard),
+    url(r'^dashboard/$', views.dashboard, name="dashboard"),
 
     # New Requests
     url(r'^requests/employee/new/$', views.NewEmployeeRequest.as_view(), name='new_employee_request'),
     url(r'^requests/contractor/new/$', views.NewContractorRequest.as_view(), name='new_contractor_request'),
-    url(r'^requests/success/$', views.request_success),
+    url(r'^requests/success/$', views.request_success, name="request_success"),
 
     # Account
     url(r'^dashboard/account/$', views.UpdateAccount.as_view(), name='update_account'),
