d43e64d7e22bc4be062c02d572457b3f371824bd
Joshua Taylor Eppinette <josheppinette@icloud.com>
2014-08-07 00:16:49 +0000

    Converted Contractors to new form.

--- app/views/contractors.py
+++ app/views/contractors.py
@@ -14,6 +14,8 @@ from django.core.urlresolvers import reverse_lazy
 
 from api.models import Contractor, Log
 
+from app.forms.contractors import NewContractorForm, UpdateContractorForm
+
 
 class ListContractors(ListView):
     """
@@ -32,44 +34,32 @@ class UpdateContractor(SuccessMessageMixin, UpdateView):
     Update an employee.
     """
     template_name = 'contractors/update.html'
+    form_class = UpdateContractorForm
     model = Contractor
     success_url = reverse_lazy('list-contractors')
     success_message = "Contractor was updated successfully!"
 
-    def get_context_data(self, **kwargs):
-        """
-        Add some stuff to context.
-        """
-        context = super(UpdateContractor, self).get_context_data(**kwargs)
-        context['name'] = '%s %s' % (self.get_object().first_name, self.get_object().last_name)
-        context['company'] = self.get_object().company.id
-        return context
 
 class NewContractor(SuccessMessageMixin, CreateView):
     """
     Create a new contractor.
     """
     template_name = 'contractors/new.html'
-    model = Contractor
+    form_class = NewContractorForm
     success_url = reverse_lazy('list-contractors')
     success_message = "Contractor creation was a success!"
 
-    def get_context_data(self, **kwargs):
-        """
-        Add some stuff to context.
-        """
-        context = super(NewContractor, self).get_context_data(**kwargs)
-        context['company'] = self.request.user.company.id
-        return context
+    def get_form_kwargs(self):
+        kwargs = super(NewContractor, self).get_form_kwargs()
+        kwargs.update({'company': self.request.user.company.pk})
+        return kwargs
 
     def form_valid(self, form):
         """
         Save the form and generate a proper log.
         """
         obj = form.save(commit=True)
-
         Log.objects.create(author=self.request.user.email, **obj.creation_log())
-
         return super(NewContractor, self).form_valid(form)
 
 
